1cadffd7147a1cbd81beb146b47583acd1862eb5
zhangwenan <jquerytech@gmail.com>
2016-02-11 21:19:04 +0800

    细节修改

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var mysql = require('mysql');
 var qqmail = require('qqmail');
 
+
 var conf_json = 'dawn.json';
 var conf = {
   "mysql_host":"",
@@ -16,6 +17,7 @@ var conf = {
   "ysdm_password":""
 };
 conf = JSON.parse(fs.readFileSync(process.cwd() + '/' + conf_json, "utf-8"));
+qqmail.init(conf);
 
 var connection = mysql.createConnection({
   host     : conf.mysql_host,
@@ -30,6 +32,9 @@ connection.query('SELECT id,qq,password from qq where status=1 order by id limit
   for(var i=0; i<rows.length; i++){
     console.log(rows[i].id, rows[i].qq, rows[i].password);
     qqmail.login(rows[i].qq, rows[i].password);
+    if(qqmail.logged_obj.login_retcode == 0){
+      connection.query('update qq set recent_login_time=now() where qq=' + rows[i].qq);
+    }
   }
 });
 
