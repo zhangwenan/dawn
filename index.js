77dcba4183949441fc4d5925082cfe64898ac4f7
zhangwenan <jquerytech@gmail.com>
2016-02-14 21:28:19 +0800

    使用async控制并发。。

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var mysql = require('mysql');
 var qqmail = require('qqmail');
+var async = require('async');
+var cookie_util = require('./lib/cookie_util.js');
 
 
 var conf_json = 'dawn.json';
@@ -13,8 +15,8 @@ var conf = {
   "softid":"",
   "softkey":"",
 
-  "ysdm_user":"",
-  "ysdm_password":""
+  "username":"",
+  "password":""
 };
 conf = JSON.parse(fs.readFileSync(process.cwd() + '/' + conf_json, "utf-8"));
 qqmail.init(conf);
@@ -27,15 +29,30 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-connection.query('SELECT id,qq,password from qq where status=1 order by id limit 1', function(err, rows, fields) {
+connection.query('SELECT id,qq,password,recent_login_time from qq where status=1 and TIMESTAMPDIFF(SECOND,recent_login_time,NOW())>24*60*60*30 order by id limit 3', function(err, rows, fields) {
   if (err) throw err;
-  for(var i=0; i<rows.length; i++){
-    console.log(rows[i].id, rows[i].qq, rows[i].password);
-    qqmail.login(rows[i].qq, rows[i].password);
-    if(qqmail.logged_obj.login_retcode == 0){
-      connection.query('update qq set recent_login_time=now() where qq=' + rows[i].qq);
-    }
-  }
+
+  async.mapLimit(rows, 1, function(temp_row, callback){
+
+
+    qqmail.login(temp_row.qq, temp_row.password, {
+      success: function(self){
+        if(self.logged_obj.login_retcode == 0){
+          connection.query('update qq set recent_login_time=now(),cookies_smart="' + cookie_util.get_simple_cookie_str(self.cookies) + '" where qq=' + temp_row.qq, function(err, rows, fields){
+            if(err) throw err;
+            console.log('【' + temp_row.id + '】' + self.qq + '状态更新成功');
+
+            callback(null, temp_row.id + '' + temp_row.qq + temp_row.password);
+          });
+        }
+      }
+    });
+
+  }, function(err, results){
+    //console.log(results);
+    connection.end();
+  });
+
 });
 
-connection.end();
+
